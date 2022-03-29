ff6ca3af3048e8ef265b4fc817215339f0d3c8dd
Dina77Ahmed <96975784+Dina77Ahmed@users.noreply.github.com>
2022-03-29 12:30:46 +0200

    Login is Done

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+   
+    <!-- Fonts -->
+     <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('admin/css/material-dashboard.css')}}" rel="stylesheet">
+
+     <!-- CSS Files -->
+  {{-- <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.0.1" rel="stylesheet" /> --}}
+</head>
+<body class="g-sidenav-show  bg-gray-200">
+  
+   @include('layouts.inc.sidebar')
+   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+      @include("layouts.inc.adminnav")
+
+   
+   <div class="container-fluid py-4">
+      <h1>Dina Ahmed </h1>
+      <div class="row">
+      @yield('row')
+      </div>
+      <div class="row">
+         @yield('row')
+         </div>
+         <div class="row mt-4">
+            @yield('row mt-4')
+            </div>
+            <div class="row mb-4">
+               @yield('row mb-4')
+               </div>
+      @include("layouts.inc.adminfooter")
+   </div>
+</main>
+
+   
+
+
+
+
+
+
+     <!-- Scripts -->
+    
+     <script src="{{ asset('admin/js/bootstrap.bundle.min') }}" defer></script>
+     <script src="{{ asset('admin/js/bootstrap.min.js') }}" defer></script>
+     <script src="{{ asset('admin/js/popper.min.js') }}" defer></script>
+     <script src="{{ asset('admin/js/perfect-scrollbar.min.js') }}" defer></script>
+     <script src="{{ asset('admin/js/smooth-scrollbar.min.js') }}" defer></script>
+     <script src="{{ asset('admin/js/chartjs.min.js') }}" defer></script>
+
+      <!--   Core JS Files   -->
+  {{-- <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/chartjs.min.js"></script> --}}
+  @yield('script')
+</body>
+</html>
